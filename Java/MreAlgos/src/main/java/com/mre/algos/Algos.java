eef161321c53215810590b10d43413bbd6ebf932
Markus Reith <mreith@gmx.net>
2020-01-22 21:23:12 +0100

    Provided Java algos for calculating primes

---
+++ Java/MreAlgos/src/main/java/com/mre/algos/Algos.java
@@ -0,0 +1,93 @@
+package com.mre.algos;
+
+import static com.mre.algos.Utils.getIntFromArgs;
+import static com.mre.algos.Utils.measurePerformance;
+
+import java.util.BitSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * This class provides various algorithms
+ * 
+ * @author Markus Reith
+ */
+public class Algos {
+    public static void main(final String[] args) {
+        final int primesUntilDefault =  1000 * 1000;
+        
+        int primesUntil = getIntFromArgs(args).orElse(primesUntilDefault);
+        final int count = measurePerformance(()->findPrimes(primesUntil), 1).size();
+        System.out.println("Found " + count + " primes below " + primesUntil);
+    }
+
+    /**
+     * Implementation of the Sieve of Eratosthenes using Streams
+     * 
+     * @param n
+     * @return List with primes below n
+     */
+    public static List<Integer> findPrimes(final int n) {
+        final var sieve = new boolean[n];
+        for (int i = 4; i < n; i += 2) {
+            sieve[i] = true;
+        }
+        return Stream
+            .iterate(2, t -> t < n, t -> t + 1)
+            .filter(i -> !sieve[i])
+            .peek(i -> {
+                for (long j = (long) i * (long) i; j < n; j += 2 * i) {
+                    sieve[(int) j] = true;
+                }
+            })
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Implementation of the Sieve of Eratosthenes using Streams and BitSet
+     * for storing the sieve
+     * 
+     * @param n
+     * @return List with primes below n
+     */
+    public static List<Integer> findPrimesBitset(final int n) {
+        final var sieve = new BitSet(n);
+        for (int i = 4; i < n; i += 2) {
+            sieve.set(i);
+        }
+        return Stream
+            .iterate(2, t -> t < n, t -> sieve
+            .nextClearBit(t + 1))
+            .peek(i -> {
+                for (long j = (long) i * (long) i; j < n; j += 2 * i) {
+                    sieve.set((int) j);
+                }
+            })
+            .collect(Collectors.toList());
+    }
+
+    /**
+     * Old school C-style implementation of the Sieve of Eratosthenes
+     * @param n
+     * 
+     * @return List with primes below n
+     */
+    public static List<Integer> findPrimesCstyle(final int n) {
+        final var sieve = new boolean[n + 1];
+        for (int i = 4; i <= n; i += 2) {
+            sieve[i] = true;
+        }
+        final var primes = new LinkedList<Integer>();
+        for (int i = 2; i < n; i++) {
+            if (!sieve[i]) {
+                primes.add(i);
+                for (long j = (long) i * (long) i; j < n; j += 2 * i) {
+                    sieve[(int) j] = true;
+                }
+            }
+        }
+        return primes;
+    }
+}
---
+++ Java/MreAlgos/src/test/java/com/mre/algos/AlgosTest.java
@@ -0,0 +1,34 @@
+package com.mre.algos;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+
+import org.junit.Test;
+
+import static com.mre.algos.Algos.*;
+import static com.mre.algos.Utils.*;
+
+public class AlgosTest {
+
+    @Test
+    public void testFindPrimes() {
+        final int primesUntil =  1000 * 1000;
+        final int countPrimesUntil1M =  78498;
+
+        var testFuncs = new ArrayList<Supplier<List<Integer>>>();
+        testFuncs.add(() -> findPrimes(primesUntil));
+        testFuncs.add(() -> findPrimesBitset(primesUntil));
+        testFuncs.add(() -> findPrimesCstyle(primesUntil));
+        var countCorrectFunctions = testFuncs
+            .stream()
+            .map(f -> measurePerformance(f, 10))
+            .map(r -> r.size())
+            .filter(n->(n==countPrimesUntil1M))
+            .count();
+    
+        assertTrue(countCorrectFunctions == testFuncs.size());
+    }
+}
