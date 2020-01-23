b2750136025c6f4deeb351f02f0ad86acc83b996
Markus Reith <mreith@gmx.net>
2020-01-23 01:03:10 +0100

    Added Spring Boot rest service

---
+++ Java/MreSpringBootServer/src/main/java/com/mre/polydev/sbserv/MreSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package com.mre.polydev.sbserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MreSpringBootServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MreSpringBootServerApplication.class, args);
+	}
+
+}
