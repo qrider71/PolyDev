34a2da799f227f515a1a320d850c9b64ac05651b
Markus Reith <mreith@gmx.net>
2020-01-26 01:24:39 +0100

    Implemented Python algo

---
+++ Python/MreAlgos/MreAlgos.py
@@ -0,0 +1,25 @@
+import time
+
+def findPrimes(until):
+    sieve = [False] * until
+    primes = [2]
+    for i in range(3, until, 2):
+        if (sieve[i] == False):
+            primes.append(i)
+            for j in range(i*i, until, i * 2):
+                sieve[j] = True
+    return primes
+
+def getMillis():
+    return int(round(time.time() * 1000))
+
+def measurePerformance(fn, n):
+    start = getMillis()
+    result = fn(n)
+    end = getMillis()
+    return (result, end-start)
+
+n =  100 * 1000 * 1000
+(primes, duration) = measurePerformance(findPrimes, n)
+nPrimes = len(primes)
+print (f'Found {nPrimes} primes until {n} in {duration} ms')
