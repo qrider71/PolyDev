24bf45cbf9bc29fe64ba77e03cfde82bc780462d
qrider71 <mreith@gmx.net>
2020-01-30 15:32:35 +0000

    work in progress

--- Fsharp/MreAlgos/MreAlgos.fs
+++ Fsharp/MreAlgos/MreAlgos.fs
@@ -1,19 +1,11 @@
-﻿// Learn more about F# at http://fsharp.org
+// Learn more about F# at http://fsharp.org
 
 open System
 
-let findPrimes m = sieve 3 (array (3,m) [(i,odd i) | i<-[3..m]] )
-  where
-    sieve :: Int -> UArray Int Bool -> [Int]
-    sieve p a
-      | p*p > m   = 2 : [i | (i,True) <- assocs a]
-      | a!p       = sieve (p+2) $ a//[(i,False) | i <- [p*p, p*p+2*p..m]]
-      | otherwise = sieve (p+2) a
-
 let findPrimes m =
     let rec sieve p a = match (p,a)
-        | (p,a) when p * p > m -> seq { for i in 1 .. n -> if a.[i] then i }
-        | (p,a) when a.[p]  -> sieve (p+2) $ a
+        | (p,a) when p * p > m -> seq { for i in 2 .. n -> if a.[i] then i }
+        | (p,a) when a.[p]  -> sieve (p+2) (a |> filterSieve (p))
         | _ -> sieve (p+2) a
     sieve 3
 
