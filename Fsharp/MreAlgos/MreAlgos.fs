81b2706ad4f03ddba368bb84f55c218fa78a6cb1
qrider71 <mreith@gmx.net>
2020-01-31 17:40:45 +0000

    F# implementation

--- Fsharp/MreAlgos/MreAlgos.fs
+++ Fsharp/MreAlgos/MreAlgos.fs
@@ -4,18 +4,35 @@ open System
 
 let findPrimes m =
 
-    let filterSieve p a =
-        for i in seq { p*p .. 2*p .. m }
-            a.[i] = false
+    let filterSieve (p:int, a:bool[]) =
+        for i in seq { p*p .. 2*p .. (m-1) } do a.[i] <- false
         a
 
-    let rec sieve p a = match (p,a)
-        | (p,a) when p * p > m -> seq { for i in 2 .. n -> if a.[i] then i }
-        | (p,a) when a.[p]  -> sieve (p+2) (a |> filterSieve (p a))
-        | _ -> sieve (p+2) a
-    sieve 3
+    let rec sieve (p:int, a:bool[]) = 
+        match (p,a) with
+        | (p,a) when p * p > m -> seq { for i in 2 .. (m-1) do if a.[i] then yield i}
+        | (p,a) when a.[p]  -> sieve ( (p+2), filterSieve (p, a) ) 
+        | _ -> sieve ((p+2), a)
+   
+    let a = Array.create m true
+    for i in 4 .. 2 .. (m-1) do a.[i] <- false
+    sieve (3, a)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
+    let timer = System.Diagnostics.Stopwatch()
+    let mDefault = 100 * 1000 * 1000
+    let m = 
+        match argv with 
+        | [||] -> mDefault
+        | [|x|] -> x |> int
+        | _ -> mDefault
+
+    timer.Start()
+    let primes = findPrimes(m)
+
+    let millis = timer.ElapsedMilliseconds
+
+    let primesCount = primes |> Seq.length
+    printfn "Found %i primes below %i in %i [ms]" primesCount m millis
     0 // return an integer exit code
