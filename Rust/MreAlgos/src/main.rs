c61dd628e8543adc317fd8c748550b61da182ffa
Markus Reith <mreith@gmx.net>
2020-02-10 00:08:33 +0100

    update

--- Rust/MreAlgos/src/main.rs
+++ Rust/MreAlgos/src/main.rs
@@ -1,3 +1,20 @@
+    
+fn find_primes(n:usize) -> Vec<usize> {
+    let mut a = vec![false; n];
+    let mut primes = Vec::new();
+    primes.push(2);
+    for i in (3..n).step_by(2) {
+        if !a[i] {
+            primes.push(i);
+            for i in (i*i..n).step_by(2*i) {
+                a[i] = true;
+            }
+        }
+    }
+    return primes;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let n = 100*1000*1000;
+    println!("{:?}", find_primes(n).len());
 }
